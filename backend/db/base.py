44504eb3d3b632012f1fc2004701ad6cb53a0c77
ahmadfarisfs <ahmadfarisfs@gmail.com>
2024-10-06 21:52:11 +0700

    bbb

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,44 +1,36 @@
 import logging
 import os
 import datetime
+from typing import Optional
 import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 import asyncio
 import sys
 
-# Configure the root logger
-logging.basicConfig(
-    level=logging.INFO,  # Set the logging level
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Format of the log message
-    handlers=[logging.StreamHandler(sys.stdout)]  # Stream handler to output to the console
-)
 
-class MongoManager:
-    client: AsyncIOMotorClient = None
+mongodb_url = os.getenv("MONGODB_URL")
+if not mongodb_url:
+    raise ValueError("MONGODB_URL environment variable not set")
+db = AsyncIOMotorClient(mongodb_url)#.get_database()
+userDB = db.users
 
-    async def connect(self):
-        if MongoManager.client is not None:
-            logging.info(f"Using existing MongoDB client in {self.__class__.__name__}.")
-            self.db = MongoManager.client.get_database()
-            return
+# class MongoManager:
+#     async def connect(self):
+#         if MongoManager.client is not None:
+#             logging.info(f"Using existing MongoDB client in {self.__class__.__name__}.")
+#             self.db = MongoManager.client.get_database()
+#             return
 
-        mongodb_url = os.getenv("MONGODB_URL")
-        if not mongodb_url:
-            raise ValueError("MONGODB_URL environment variable not set")
+#         mongodb_url = os.getenv("MONGODB_URL")
+#         if not mongodb_url:
+#             raise ValueError("MONGODB_URL environment variable not set")
         
-        logging.info(f"Connecting to MongoDB in {self.__class__.__name__}.")
-        MongoManager.client = AsyncIOMotorClient(mongodb_url)
-        self.db = MongoManager.client.get_database()
-        logging.info(f"Connected to MongoDB in {self.__class__.__name__}.")
+#         logging.info(f"Connecting to MongoDB in {self.__class__.__name__}.")
+#         MongoManager.client = AsyncIOMotorClient(mongodb_url)
+#         self.db = MongoManager.client.get_database()
+#         logging.info(f"Connected to MongoDB in {self.__class__.__name__}.")
 
-    async def close(self):
-        logging.info("Closing connection with MongoDB.")
-        MongoManager.client.close()
-        MongoManager.client = None
-        logging.info("Closed connection with MongoDB.")
-
-
-
-# mon = MongoManager()
-# asyncio.run(mon.connect_to_database())
-# asyncio.run(mon.add_user("koko","dsad","ppp2d2"))
+#     async def close(self):
+#         logging.info("Closing connection with MongoDB.")
+#         MongoManager.client.close()
+#         logging.info("Closed connection with MongoDB.")
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-# import sqlite3
-from backend.routers import user
+import uvicorn
+from routers.user import router as UserRouter
 import logging
+import sys
+
+# Configure the root logger
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    format='%(asctime)s - %(levelname)s - %(message)s',  
+    handlers=[logging.StreamHandler(sys.stdout)]  
+)
+
 app = FastAPI()
-logging.info("Staring!")
-# Include the routers
-app.include_router(user.router)
-# app.include_router(expense.router)
+app.include_router(UserRouter)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
