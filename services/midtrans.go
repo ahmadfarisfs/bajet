a55bc654ec39d056effb1b2ae62a532e0eea04e9
ahmadfarisfs <ahmadfarisfs@gmail.com>
2024-11-29 08:24:49 +0700

    wip: midtrans callback handler

--- services/midtrans.go
+++ services/midtrans.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/midtrans/midtrans-go"
@@ -25,7 +27,56 @@ func NewMidtransMoneyReceiver(serverKey string, isSandbox bool) *MidtransReceive
 	}
 }
 
-func (r *MidtransReceiver) HandleNotificationCallback()
+func (r *MidtransReceiver) GetNotificationCallbackHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		// 1. Initialize empty map
+		var notificationPayload map[string]interface{}
+
+		// 2. Parse JSON request body and use it to set json to payload
+		err := json.NewDecoder(req.Body).Decode(&notificationPayload)
+		if err != nil {
+			// do something on error when decode
+			return
+		}
+
+		// 3. Get order-id from payload
+		orderId, exists := notificationPayload["order_id"].(string)
+		if !exists {
+			// do something when key `order_id` not found
+			return
+		}
+
+		// 4. Check transaction to Midtrans with param orderId
+		transactionStatusResp, e := r.client.CheckTransaction(orderId)
+		if e != nil {
+			http.Error(w, e.GetMessage(), http.StatusInternalServerError)
+			return
+		} else {
+			if transactionStatusResp != nil {
+				// 5. Do set transaction status based on response from check transaction status
+				if transactionStatusResp.TransactionStatus == "capture" {
+					if transactionStatusResp.FraudStatus == "challenge" {
+						// TODO set transaction status on your database to 'challenge'
+						// e.g: 'Payment status challenged. Please take action on your Merchant Administration Portal
+					} else if transactionStatusResp.FraudStatus == "accept" {
+						// TODO set transaction status on your database to 'success'
+					}
+				} else if transactionStatusResp.TransactionStatus == "settlement" {
+					// TODO set transaction status on your database to 'success'
+				} else if transactionStatusResp.TransactionStatus == "deny" {
+					// TODO you can ignore 'deny', because most of the time it allows payment retries
+					// and later can become success
+				} else if transactionStatusResp.TransactionStatus == "cancel" || transactionStatusResp.TransactionStatus == "expire" {
+					// TODO set transaction status on your database to 'failure'
+				} else if transactionStatusResp.TransactionStatus == "pending" {
+					// TODO set transaction status on your database to 'pending' / waiting payment
+				}
+			}
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("ok"))
+	}
+}
 
 func (r *MidtransReceiver) CreateDynamicQRISInvoice(amount int) (*coreapi.ChargeResponse, error) {
 	req := &coreapi.ChargeReq{
