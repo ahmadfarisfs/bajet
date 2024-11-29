d4070c4d3eb3e76fecfdbba5a97eadd8ab22eb83
ahmadfarisfs <ahmadfarisfs@gmail.com>
2024-11-29 08:29:24 +0700

    chore: refactor

--- services/midtrans.go
+++ services/midtrans.go
@@ -51,28 +51,32 @@ func (r *MidtransReceiver) GetNotificationCallbackHandler() http.HandlerFunc {
 		if e != nil {
 			http.Error(w, e.GetMessage(), http.StatusInternalServerError)
 			return
-		} else {
-			if transactionStatusResp != nil {
-				// 5. Do set transaction status based on response from check transaction status
-				if transactionStatusResp.TransactionStatus == "capture" {
-					if transactionStatusResp.FraudStatus == "challenge" {
-						// TODO set transaction status on your database to 'challenge'
-						// e.g: 'Payment status challenged. Please take action on your Merchant Administration Portal
-					} else if transactionStatusResp.FraudStatus == "accept" {
-						// TODO set transaction status on your database to 'success'
-					}
-				} else if transactionStatusResp.TransactionStatus == "settlement" {
-					// TODO set transaction status on your database to 'success'
-				} else if transactionStatusResp.TransactionStatus == "deny" {
-					// TODO you can ignore 'deny', because most of the time it allows payment retries
-					// and later can become success
-				} else if transactionStatusResp.TransactionStatus == "cancel" || transactionStatusResp.TransactionStatus == "expire" {
-					// TODO set transaction status on your database to 'failure'
-				} else if transactionStatusResp.TransactionStatus == "pending" {
-					// TODO set transaction status on your database to 'pending' / waiting payment
-				}
-			}
 		}
+
+		if transactionStatusResp == nil {
+			//TODO: handle error
+		}
+
+		// 5. Do set transaction status based on response from check transaction status
+		if transactionStatusResp.TransactionStatus == "capture" {
+			if transactionStatusResp.FraudStatus == "challenge" {
+				// TODO set transaction status on your database to 'challenge'
+				// e.g: 'Payment status challenged. Please take action on your Merchant Administration Portal
+			} else if transactionStatusResp.FraudStatus == "accept" {
+				// TODO set transaction status on your database to 'success'
+			}
+		} else if transactionStatusResp.TransactionStatus == "settlement" {
+			// TODO set transaction status on your database to 'success'
+		} else if transactionStatusResp.TransactionStatus == "deny" {
+			// TODO you can ignore 'deny', because most of the time it allows payment retries
+			// and later can become success
+		} else if transactionStatusResp.TransactionStatus == "cancel" || transactionStatusResp.TransactionStatus == "expire" {
+			// TODO set transaction status on your database to 'failure'
+		} else if transactionStatusResp.TransactionStatus == "pending" {
+			// TODO set transaction status on your database to 'pending' / waiting payment
+
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte("ok"))
 	}
