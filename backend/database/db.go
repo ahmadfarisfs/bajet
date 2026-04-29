325e70d311db7207991a8a7d73b0d6a14acc0b3a
ahmadfarisfs <ahmadfarisfs@gmail.com>
2026-04-29 20:08:25 +0700

    feat: update database initialization to support PostgreSQL and remove unused backend trigger

--- backend/database/db.go
+++ backend/database/db.go
@@ -1,7 +1,11 @@
 package database
 
 import (
+	"os"
+	"strings"
+
 	"github.com/ahmadfarisfs/bajet/models"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -11,9 +15,14 @@ var DB *gorm.DB
 
 func Init(path string) error {
 	var err error
-	DB, err = gorm.Open(sqlite.Open(path), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
+	cfg := &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if strings.HasPrefix(dbURL, "postgres") {
+		DB, err = gorm.Open(postgres.Open(dbURL), cfg)
+	} else {
+		DB, err = gorm.Open(sqlite.Open(path), cfg)
+	}
 	if err != nil {
 		return err
 	}
